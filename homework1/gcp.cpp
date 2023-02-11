8ab2e783e28c124a419a1da86f02f2d3f7b68cf0
Ubuntu <ubuntu@ip-172-31-59-120.ec2.internal>
2023-02-11 23:26:26 +0000

    my first cpp problem

---
+++ homework1/gcp.cpp
@@ -0,0 +1,40 @@
+/* 
+ * gcd.cpp
+ * author:Michael Castaneda
+ * class:CISC 7124
+ * GCD definition:The Greatest Common Divisor of two positive integers (a,b) is the largest divisor common to both a and b.
+ */
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+class Gcd {
+	public:
+		int x, y,result;
+
+		Gcd(int x, int y) :x(x), y(y) {}
+	
+		int calc_gcd(int x,int y) {
+			if( x == 0 || y ==0 )
+				return 0;
+			else if ( x ==y )
+				return y;
+			else if ( x > y)
+				return __gcd(y-x,y);
+			else
+				return __gcd(x,y-x);
+		};
+	
+		void print() {
+			std::cout << "The greatest common divisor of "<< x << " and " << y << " is " <<calc_gcd(x,y) << std::endl;
+		};
+};
+
+int main(){
+	Gcd p(213,2354);
+	p.print();
+
+	return 0;
+}
