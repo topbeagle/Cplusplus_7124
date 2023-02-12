aa4e1dc3ca52a76728fb3be88f89ad4ef82d1e0d
Ubuntu <ubuntu@ip-172-31-5-102.ec2.internal>
2023-02-12 23:15:19 +0000

    started hw1 part 2 p3

---
+++ homework1/sorted.cpp
@@ -0,0 +1,22 @@
+/*
+ * sorted.cpp
+ * author:Michael Castaneda
+ * class:CISC 7124
+ * Test if array a is sorted in ascending order. 
+ */
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+class random {
+	public:
+		srand(time(0));
+
+int main() {
+
+	
+	return 0;
+}
+
