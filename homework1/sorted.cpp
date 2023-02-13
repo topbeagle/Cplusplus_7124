fa24f721cb8b7bfd6eef42c4e40991218f52bf4f
Ubuntu <ubuntu@ip-172-31-51-45.ec2.internal>
2023-02-13 05:05:40 +0000

    further on p3

--- homework1/sorted.cpp
+++ homework1/sorted.cpp
@@ -8,15 +8,68 @@
 #include <cstdlib>
 #include <ctime>
 
+
 using namespace std;
 
 class random {
+	private:
+		int arr_size = 10;
+		int limit = 100;
+
+	public:	
+
+		int*  random_calc() {
+			
+			srand(time(0));
+
+			int* arr = new int[arr_size];
+
+			for(int i=0; i <arr_size;i++)
+				arr[i] = rand() % limit+1;
+
+			std::cout << "New array created  " << arr << std::endl; 
+
+			return arr;
+		}
+};
+
+
+class ascTest {
 	public:
-		srand(time(0));
+		random Ra;
+
+		int* unsort_arr;
+		int* sorted_arr;
+ 
+		bool ascTest_calc() {
+			int size;
+			int* copy_arr;
+
+			int* unsort_arr = Ra.random_calc();
+
+			size = sizeof(unsort_arr);
+
+			std::sort(unsort_arr);
+
+			for(int i=0;i<size;i++)
+				copy_arr[i] = unsort_arr[i];
+
+
+			/* test if each element array matches */ 
+			for(int n=0;n<size;n++)
+			       for(int j=0;j<size;j++)
+			      	   if ( copy_arr[n] != unsort_arr[j])
+				    	return false;
+
+			return true;
+		}
+};
 
 int main() {
 
-	
+	ascTest A;
+	A.ascTest_calc();
+
 	return 0;
 }
 
