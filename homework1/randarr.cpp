9a6196c31c3cc988687f5619e75f0b46a5896d4c
Ubuntu <ubuntu@ip-172-31-59-198.ec2.internal>
2023-02-23 04:29:30 +0000

    progress

--- homework1/randarr.cpp
+++ homework1/randarr.cpp
@@ -2,38 +2,30 @@
 /* is a preprocessor directive in C++ that tells the compiler to include the contents of the iostream header file in the current source file. The iostream header file provides functionality for input and output operations in C++, including reading input from the keyboard and displaying output to the screen.
 */
 #include <ctime>
-#include <random>
-/*
- * The Mersenne Twister is a general-purpose pseudorandom number generator developed in 1997 by Makoto Matsumoto and Takuji Nishimura. Its name derives from the fact that its period length is chosen to be a Mersenne prime.
-*/
+#include <cstdlib>
+
 using namespace std;
+/* is a hack that save you from writing 'std::something */
 
 class myrand {
 	public:
-		const int arrsize;
-		int arr[];
+
+		myrand(int arr[],int size) {
 		/* a constructor is a special member function of a class that is used to initialize its objects. Constructors have the same name as the class, and they are called automatically when an object of the class is created. */
-
-		myrand(int a[],int as){
-			arrsize = as;
-			arr[arrsize] = a;
+			for(int i = 0; i < size; i++)
+				arr[i] = {0};
 		}
 
+		int* randcalc(int arr[],int size) {
+			srand(time(0));
 		
+			for( int i = 0;i<size;i++) {
+				int randomNumber = rand() % 100 +1;
+				arr[i] = randomNumber;
+				cout << arr[i]<< endl;
+			}	
 
-		int* randcalc(int arr[],int arrsize) {
-			/* Use the system's random device to obtain a seed */
-			std::random_device rd;
-			/*Use the Mersenne Twister engine with the seed */
-			std::uniform_int_distribution<int> distr(1, 100);	
-		
-			for( int i;i>arrsize;i++) {
-			/* Generate a random number using the generator */
-			int random_number = distr(gen);
-			arr[i] = random_number;
-		}
-
-		return arr;
+			return arr;
 		}
 };
 
@@ -44,9 +36,10 @@ int main() {
 	const int arrsize = 10;
 	int arr[arrsize] = {0};
 
-	myrand R;
+	myrand R(arr,arrsize);
 
 	R.randcalc(arr,arrsize);
+
 
 	return 0;
 }
