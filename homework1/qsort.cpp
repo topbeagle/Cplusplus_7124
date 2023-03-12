73567ba706ce972e07e48929f36e70f3ac42b9ba
Ubuntu <ubuntu@ip-172-31-53-255.ec2.internal>
2023-03-12 22:17:55 +0000

    save qsort

---
+++ homework1/qsort.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+class qSort {
+  public:
+	  
+	  int fe() {
+
+            int temp = 0;
+	    //vector<int> s = 
+	    vector<int> arr = { 23, 101, 1, 34, 22, 67, 20, 300};
+
+  	    for (int arrs : arr ) {
+	      cout << "Element : " << arrs << endl;
+	      if ( temp < arr) {
+		     ;
+	      else
+		     temp arr 
+
+	    }
+
+	    return 12;
+	 } 	    
+
+};
+
+
+int main(void) {
+
+  int num;
+
+  qSort Q;
+
+  num = Q.fe();
+
+  return 0;
+}
+
+
